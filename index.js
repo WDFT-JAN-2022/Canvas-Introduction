f004e4b8aec8f3805711760199abca85bd826f97
MikeTroianello <mike.troianello@gmail.com>
2022-01-20 11:01:29 -0800

    added sprite/background

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const ctx = canvas.getContext("2d");
 canvas.height = 600;
 canvas.width = 600;
 
+const img = new Image();
+
+img.src = "./mario.png";
+
+img.onload = () => {
+  ctx.drawImage(img, 100, 100, 40, 50);
+};
+
 // ctx.fillRect(x,y,w,h)
 
-ctx.fillRect(100, 100, 100, 100);
-ctx.fillStyle = "blue";
+// ctx.fillRect(100, 100, 100, 100);
+// ctx.fillStyle = "blue";
 
-ctx.fillRect(400, 400, 100, 100);
+// ctx.fillRect(400, 400, 100, 100);
 
-ctx.strokeRect(250, 250, 100, 100);
+// ctx.strokeRect(250, 250, 100, 100);
+
+// ctx.drawImage(img, x,y,w,h)
+// ctx.drawImage(img, 100, 100, 40, 40);
